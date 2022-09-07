13ffaefabdab1d7f134caf06e569c1619b6c6b2a
akashiyer <akash1305.ai@gmail.com>
2022-09-07 13:54:56 +1000

    App now successfully working on both frontend and backend, homepage almost done up

--- src/app/pages/dashboard/dashboard-routing.module.ts
+++ src/app/pages/dashboard/dashboard-routing.module.ts
@@ -6,7 +6,31 @@ import {DashboardPage} from './dashboard.page';
 const routes: Routes = [
   {
     path: '',
-    component: DashboardPage
+    component: DashboardPage,
+    children: [
+      {
+        path: 'tab1',
+        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+      },
+      {
+        path: 'tab2',
+        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+      },
+      {
+        path: 'tab3',
+        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard/tab1',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard/tab1',
+    pathMatch: 'full'
   }
 ];
 
