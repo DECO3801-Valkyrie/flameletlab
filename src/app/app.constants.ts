fc36ad79c54f4848172d25f747869e06e60e01b9
Chol Nhial <chol.nhial.chol@gmail.com>
2022-10-14 15:32:02 +1000

    prepare for deployment

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,4 +1,6 @@
-export const SERVER_API_URL = 'http://localhost:8080/';
+import {environment} from '../environments/environment';
+
+export const SERVER_API_URL = `http://${environment.apiServer}:8080/`;
 export const GOOGLE_PLACES_API_KEY = 'AIzaSyCWfP_3D3jCTlXdMvHQa8Y_r0r_iov83Yc';
 export const DEFAULT_RADIUS = 3200000;
 
