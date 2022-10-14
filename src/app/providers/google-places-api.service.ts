48dc8ce1d37bdc0e85874c3e6a2e836ef1966a22
Chol Nhial <chol.nhial.chol@gmail.com>
2022-10-14 16:32:11 +1000

    proxy places api

--- src/app/providers/google-places-api.service.ts
+++ src/app/providers/google-places-api.service.ts
@@ -1,4 +1,4 @@
-import {DEFAULT_RADIUS, GOOGLE_PLACES_API_KEY} from '../app.constants';
+import {GOOGLE_PLACES_API_KEY, SERVER_API_URL} from '../app.constants';
 import {HttpClient, HttpResponse} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {IAccount} from '../model/account';
@@ -8,14 +8,13 @@ import {Injectable} from '@angular/core';
   providedIn: 'root'
 })
 export class GooglePlacesApiService {
-  public url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${GOOGLE_PLACES_API_KEY}&radius=${DEFAULT_RADIUS}&strictbounds=true&location=-25.274398%2C133.775136&components=country:au`;
 
   constructor(public http: HttpClient) {
   }
 
 
   getSearchResults(query): Observable<HttpResponse<any>> {
-    return this.http.get<IAccount>(`${this.url}&input=${query}`, { observe: 'response' });
+    return this.http.get<IAccount>(`${SERVER_API_URL}places-api/query?query=${query}`, { observe: 'response' });
   }
 
   getPlaceByPlaceId(placeId): Observable<HttpResponse<any>> {
